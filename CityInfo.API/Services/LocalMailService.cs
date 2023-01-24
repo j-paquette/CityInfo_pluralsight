a652e2057f5fa234f757f9993c154d5606ed88cc
j-paquette <56076749+j-paquette@users.noreply.github.com>
2023-01-24 14:34:34 -0500

    finished chapter 5

--- CityInfo.API/Services/LocalMailService.cs
+++ CityInfo.API/Services/LocalMailService.cs
@@ -3,8 +3,20 @@
     //to add an interface, hghlight the LocalMailService, then right-click & choose Quick Actions &Refactorings-->extract to Interface
     public class LocalMailService : IMailService
     {
-        private string _mailTo = "admin@mycompany.com";
-        private string _mailFrom = "noreply@mycompany.com";
+        //Initialize these fields, to be sure the values are coming from the appsettings.json
+        //Make them readonly to prevent them from being changed
+        private readonly string _mailTo = string.Empty;
+        private readonly string _mailFrom = string.Empty;
+
+        //inject the IConfig service to access it from LocalMailService
+        public LocalMailService(IConfiguration configuration)
+        {
+            //read the 2 values from appsettings.json and assign them through the indexer and pass the key:value pair
+            //access is done by heirarchy: mailSettings:...
+            //The keys are case-insensitive
+            _mailTo = configuration["mailSettings:mailToAddress"];
+            _mailFrom = configuration["mailSettings:mailFromAddress"];
+        }
 
         public void Send(string subject, string message)
         {
