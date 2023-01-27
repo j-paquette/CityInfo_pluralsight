94c95b2dfe5bd316109ee16600c32a46aca06310
j-paquette <56076749+j-paquette@users.noreply.github.com>
2023-01-27 18:06:20 -0500

    still wip, continue with authentication

---
+++ CityInfo.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        //ensure the class that contains the username/password should NOT be used outside of this controller.
+        public class AuthenticationRequestBody
+        {
+            public string? UserName { get; set; }
+            public string? Password { get; set; }
+        }
+
+        private class CityInfoUser
+        {
+            public int UserId { get; set; }
+            public string UserName { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string City { get; set; }
+
+            public CityInfoUser(
+                int userId,
+                string userName,
+                string firstName,
+                string lastName,
+                string city)
+            {
+                UserId = userId;
+                UserName = userName;
+                FirstName = firstName;
+                LastName = lastName;
+                City = city;
+            }
+
+        }
+        //Access configuration values, by injecting a config object
+        //Do this by constructor injection
+        public AuthenticationController(IConfiguration configuration)
+        {
+            _configuration = configuration ??
+                throw new ArgumentNullException(nameof(configuration));
+        }
+
+        //Accept AuthenticationRequestBody as input
+        //It will automatically be deserialized, because it's a complex object
+        [HttpPost("authenticate")]
+        public ActionResult<string> Authenticate(
+            AuthenticationRequestBody authenticationRequestBody)
+        {
+            // Step 1: validate the username/password
+            var user = ValidateUserCredentials(
+                authenticationRequestBody.UserName,
+                authenticationRequestBody.Password);
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            //Step 2: createa a token
+            var securityKey = new SymmetricSecurityKey(
+                Encoding.ASCII.GetBytes(_configuration["Authentication:SecretForKey"]));
+            var signingCredentials = new SigningCredentials(
+                securityKey, SecurityAlgorithms.HmacSha256);
+
+            //This creates a Claim: key-value pairs
+            //identity-related info on the user
+            //Sub is a standardized key for the unique user identifier
+            var claimsForToken = new List<Claim>();
+            claimsForToken.Add(new Claim("sub", user.UserId.ToString()));
+            claimsForToken.Add(new Claim("given_name", user.FirstName));
+            claimsForToken.Add(new Claim("family_name", user.LastName));
+            claimsForToken.Add(new Claim("city", user.City));
+
+            //Create the token
+            //Pass thru the issuer & audience values
+            //Then pass thru the claims
+            //The first date token indicates the start of token validity (before this time the token validation will fail)
+            //The second date token indicates the end of token validity (after this time the token is no longer valid)
+            //Then pass thru the signing credentials we just created
+            var jwtSecurityToken = new JwtSecurityToken(
+                _configuration["Authentication:Issuer"],
+                _configuration["Authentication:Audience"],
+                claimsForToken,
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddHours(1),
+                signingCredentials);
+
+            //To write the token
+            //Pass thru the JwtSecurityToken we juste created
+            var tokenToReturn = new JwtSecurityTokenHandler()
+               .WriteToken(jwtSecurityToken);
+
+            //Return the token string
+            return Ok(tokenToReturn);
+
+        }
+
+        //Usually user login info is stored in a separate db, but out of scope for this course
+        private CityInfoUser ValidateUserCredentials(string? userName, string? password)
+        {
+            // we don't have a user DB or table.  If you have, check the passed-through
+            // username/password against what's stored in the database.
+            //
+            // For demo purposes, we assume the credentials are valid
+
+            // return a new CityInfoUser (values would normally come from your user DB/table)
+            return new CityInfoUser(
+                1,
+                userName ?? "",
+                "Kevin",
+                "Dockx",
+                "Antwerp");
+
+        }
+    }
+}
